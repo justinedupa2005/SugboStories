5ad477e427d342f4937734d39a60c39e3c61f4dd
justinedupa2005 <justinedupa2005@gmail.com>
2026-05-11 17:00:52 +0800

    Finish creating all ui pages and routing

--- SugboStories/Controllers/HomeController.cs
+++ SugboStories/Controllers/HomeController.cs
@@ -6,15 +6,13 @@ namespace SugboStories.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
-        {
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
+        public IActionResult Index() => View();
+        public IActionResult Stories() => View();
+        public IActionResult Sites() => View();
+        public IActionResult Archives() => View();
+        public IActionResult Timeline() => View();
+        public IActionResult About() => View();
+        public IActionResult Privacy() => View();
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
